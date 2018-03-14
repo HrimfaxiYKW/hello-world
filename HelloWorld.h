9def85ad0e71a3a76f9e23b71dc7323bea50bc75
HrimfaxiYKW <37138887+HrimfaxiYKW@users.noreply.github.com>
2018-03-14 20:34:39 +0800

    Update HelloWorld.h

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,6 +1,14 @@
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
+
+#include <iostream>
+
+using namespace std;
+
 class HelloWorld {
-  
+    public:
+        void print(){
+            cout << "Hello, world." << endl;
+        }
 };
 #endif
